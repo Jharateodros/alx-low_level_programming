ca6b716c9ff22e5c4e86d01102252e578eb4018b
Jharateodros <jhatedy03@gmail.com>
2022-12-13 06:16:35 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void prints_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+
+	return (0);
+}
