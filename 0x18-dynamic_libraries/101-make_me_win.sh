117e37c8e8dc12f67b8a340d0cd72a54824e1c81
Jharateodros <jhatedy03@gmail.com>
2023-03-20 10:20:53 -0700

    task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/winwin.so https://github.com/ermiasbahru/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
-export LD_PRELOAD=/tmp/winwin.so
+wget -P /tmp -q "https://github.com/pforciol/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/libmmw.so"
+export LD_PRELOAD="/tmp/libmmw.so"
